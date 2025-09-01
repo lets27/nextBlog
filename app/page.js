b5ffaf61ab5d12ea6a84752821ab8dd07a33d8af
LETSO27 <letsomogwera61@gmail.com>
2025-09-01 11:27:31 +0200

    removed revalidate in page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import BlogList from "@/components/blogList";
 import { getBlogs } from "./actions";
-export const revalidate = 60;
+
 export default async function Home() {
   const blogs = await getBlogs();
   return (
